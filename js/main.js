88100eef636b16949194ec717a231e180b04d262
Kendall Bendheim <kendall.bendheim@gmail.com>
2020-07-10 18:03:43 +0200

    Almost everything
    Nearly done

---
+++ js/main.js
@@ -0,0 +1,8 @@
+lightbox.option({
+    'resizeDuration': 600,
+    'wrapAround': true,
+    'showImageNumberLabel': false,
+    'alwaysShowNavOnTouchDevices': true,
+    positionFromTop: 100
+   
+  });
