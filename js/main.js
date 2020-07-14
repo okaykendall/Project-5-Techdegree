7d3ad2fb5ca9a78faa6a68dee66f0aeb87f3a3be
Kendall Bendheim <kendall.bendheim@gmail.com>
2020-07-14 14:35:03 +0200

    Added search
    search with issues still

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ lightbox.option({
     positionFromTop: 100
    
   });
+
+  $(document).ready(function() {
+	$('#search').hideseek();
+});
+
+document.getElementById("search").addEventListener("keyup", searchFunction);
+
+function searchFunction() {
+    let input = document.getElementById("search").nodeValue.toLowerCase();
+    let images = document.querySelectorAll(".gallery a");
+    let caption = document.getAttribute("data-title").nodeValue.toLowerCase();
+    for (let i = 0; i < images.length; i ++) {
+        let data = images[i];
+        caption = data.getAttribute("data-title").toLowerCase();
+        if (caption.includes(input)) {
+            images[i].style.display = "";
+        } else {
+            images[i].style.display = "none";
+        }
+    }
+
+}
